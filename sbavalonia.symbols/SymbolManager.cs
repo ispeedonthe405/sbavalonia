783f99eda1858b6d0ae19408d779748f04d9190b
ispeedonthe405 <ispeedonthe405@gmail.com>
2025-06-19 17:50:29 -0700

    Exposed symbol colors for Light and Dark themes as dynamic properties

--- sbavalonia.symbols/SymbolManager.cs
+++ sbavalonia.symbols/SymbolManager.cs
@@ -7,19 +7,39 @@ namespace sbavalonia.symbols
     {
         private static Application? _App;
         public static event EventHandler? SymbolColorChanged;
-        private static Color _Symbol_Light = new(255, 227, 227, 227);
-        private static Color _Symbol_Dark = new(255, 31, 31, 31);
+        private static Color _Symbols_LightTheme = new(255, 31, 31, 31);
+        private static Color _Symbols_DarkTheme = new(255, 227, 227, 227);
+
+
+        /// <summary>
+        /// This can be changed at any time
+        /// </summary>
+        public static Color LightThemeColor 
+        {
+            get => _Symbols_LightTheme;
+            set { _Symbols_LightTheme = value; UpdateColor(); OnSymbolColorChanged(); }
+        }
+
+        
+        /// <summary>
+        /// This can be changed at any time
+        /// </summary>
+        public static Color DarkThemeColor
+        {
+            get => _Symbols_DarkTheme;
+            set { _Symbols_DarkTheme = value; UpdateColor(); OnSymbolColorChanged(); }
+        }
 
         public static void OnSymbolColorChanged()
         {
             SymbolColorChanged?.Invoke(null, EventArgs.Empty);
         }
 
-        private static Color _SymbolColor = _Symbol_Dark;
+        private static Color _SymbolColor = _Symbols_LightTheme;
         public static Color SymbolColor
         {
             get => _SymbolColor;
-            set
+            private set
             {
                 if (value != _SymbolColor)
                 {
@@ -29,10 +49,27 @@ namespace sbavalonia.symbols
             }
         }
 
-        public static void Integrate(Application app)
+        /// <summary>
+        /// This function initializes the SymbolManager. Call this early in the 
+        /// Application class lifetime, such as within the overridden 
+        /// Application.Initialize().
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="lightThemeColor"></param>
+        /// <param name="darkThemeColor"></param>
+        public static void Integrate(Application app, Color? lightThemeColor = null, Color? darkThemeColor = null)
         {
             _App = app;
             _App.ActualThemeVariantChanged += App_ActualThemeVariantChanged;
+
+            if (lightThemeColor is not null)
+            {
+                LightThemeColor = lightThemeColor.Value;
+            }
+            if (darkThemeColor is not null)
+            {
+                DarkThemeColor = darkThemeColor.Value;
+            }
 
             UpdateColor();
         }
@@ -46,11 +83,11 @@ namespace sbavalonia.symbols
         {
             if (_App!.ActualThemeVariant.Equals(Avalonia.Styling.ThemeVariant.Light))
             {
-                SymbolColor = _Symbol_Dark;
+                SymbolColor = LightThemeColor;
             }
             else
             {
-                SymbolColor = _Symbol_Light;
+                SymbolColor = DarkThemeColor;
             }
         }
     }
